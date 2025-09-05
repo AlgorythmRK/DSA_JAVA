0ae056772f40ddc795d1c969c3fc327da82016ce
AlgorythmRK <royalraghu53@gmail.com>
2025-09-05 09:50:10 +0530

    init commit

--- Patterns.java
+++ Patterns.java
@@ -113,5 +113,21 @@ public class Patterns {
             }
             System.out.println();
         }
+        System.out.println();
+        //Boolean Traingle
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++) {
+                if ((i + j) % 2 == 0)
+                {
+                    System.out.print("0");
+                }
+                else
+                {
+                    System.out.print("1");
+                }
+            }
+            System.out.println();
+        }
     }
 }
